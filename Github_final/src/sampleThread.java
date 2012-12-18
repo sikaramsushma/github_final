ce880120c0cda333c73e6b62dedd057b5805eba6
ssikaram <ssikaram@hasowntaste-l7.y.corp.yahoo.com>
2012-12-18 14:09:36 +0530

    "testing for cloning"

--- Github_final/src/sampleThread.java
+++ Github_final/src/sampleThread.java
@@ -4,7 +4,9 @@ public class sampleThread implements Runnable {
 	
 	public static void main(String[] args) {
 		Thread thre = new Thread(new sampleThread(), "SampleThread");
-System.out.println("test");
+		System.out.println("test");
+		System.out.println("test clone");
+
 		thre.start();
 		
 	}
